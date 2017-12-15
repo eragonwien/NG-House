a9049e3605048118886ea7a7109a533904c85928
SonLaptop\erago <eragonwien@gmail.com>
2017-12-15 11:48:48 +0100

    add node-sass

--- ng-house-api/app.js
+++ ng-house-api/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sass = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var houses = require('./routes/houses');
@@ -15,15 +16,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//SASS
+app.use(
+	sass({
+		src: __dirname + '/sass', 
+     	dest: __dirname + '/public/stylesheets',
+        prefix:  '/stylesheets',
+		debug: true
+	})
+);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/houses', houses);
+app.use('/ng-house', houses);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
