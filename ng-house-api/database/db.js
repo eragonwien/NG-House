024df29ec7b6ed2599cb714c6fdf77850fe00dcf
SonLaptop\erago <eragonwien@gmail.com>
2017-12-18 21:58:39 +0100

    change to connect directly from pool

--- ng-house-api/database/db.js
+++ ng-house-api/database/db.js
@@ -1,29 +1,30 @@
-var connector = require('./dbconnector');
+var mysql = require('mysql');
+var dbConfig = require('./dbConfig');
+exports.pool_test = mysql.createPool({
+  host     : dbConfig.HOST,
+  user     : dbConfig.USER,
+  password : dbConfig.PASSWORD,
+  database : dbConfig.DB_TEST,
+  connectionLimit: dbConfig.LIMIT
+});
 
-var pool = connector.pool;
+exports.pool_production = mysql.createPool({
+  host     : dbConfig.HOST,
+  user     : dbConfig.USER,
+  password : dbConfig.PASSWORD,
+  database : dbConfig.DB_PRODUCTION,
+  connectionLimit: dbConfig.LIMIT
+});
 
-exports.query = function(cmd, params, callback){
-	pool.getConnection(function(err, connection){
-		if (err) {
-			console.log(err);
-			callback(true, null);
-			return;
-		}
-		//console.log("connected");
+exports.pool_no_database = mysql.createPool({
+  host     : dbConfig.HOST,
+  user     : dbConfig.USER,
+  password : dbConfig.PASSWORD,
+  connectionLimit: dbConfig.LIMIT
+});
 
-		connection.query(cmd, params, function(error, results, fields){
-			connection.release();
-
-			if (error) {
-				console.log(error);
-				callback(true, null);
-				return;
-			}
-			callback(false, results);
-		});
+exports.closePool = function() {
+	pool.end(function(){
+		console.log('Pool closed');
 	});
-}
-
-exports.donothing = function(){
-	return "nothing";
 }
