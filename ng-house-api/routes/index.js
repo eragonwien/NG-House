50cc09110ec86a0b664bfd87eafa75c864d0d02e
SonLaptop\erago <eragonwien@gmail.com>
2017-12-18 22:00:01 +0100

    redirect start page to \ng-house

--- ng-house-api/routes/index.js
+++ ng-house-api/routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	if (true) {
-		res.render('index', {title: 'NG-House'});
-	}
-	else {
-		res.redirect('/ng-house');
-	}
-  
+  	res.redirect('/ng-house');
 });
 
 module.exports = router;
