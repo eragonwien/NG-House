cb6b36e74c21a2cdd59d8d9666ca5f817693f10f
SonLaptop\erago <eragonwien@gmail.com>
2017-12-20 21:46:32 +0100

    refaactor controller & add input validation for filter

--- ng-house-api/public/javascripts/houseController.js
+++ ng-house-api/public/javascripts/houseController.js
@@ -11,19 +11,25 @@ angular
 
 		$scope.toggleAdding = function(){
 			$scope.addNewHouse = !($scope.addNewHouse);
-			console.log($scope.addNewHouse);
 		};
 
 		$scope.newHouse = {};
 		$scope.addHouse = function(){
-			if (!$scope.newHouse || $scope.newHouse.length <= 0) {
-				return;
+
+			if (houseService.houseInputIsValid($scope.newHouse)) {
+				saveHouse();
+				$scope.setAlert(false, $scope.newHouse.Type + " " + $scope.newHouse.ID + " added.");
+			}
+			else {
+				console.log("Invalid Input. Adding aborted !");
+				$scope.setAlert(true, "Invalid Input. Adding aborted !");
 			}
 
-			if (!houseService.houseInputIsValid($scope.newHouse)) {
-				return;
-			}
+			$scope.newHouse = {};
+			$scope.addNewHouse = false;
+		};
 
+		function saveHouse() {
 			if (typeof $scope.newHouse.Image === 'undefined') {
 				$scope.newHouse.Image = 'default.jpg';
 			}
@@ -40,9 +46,9 @@ angular
 
 			houseResource.$save(function(){
 				$scope.houses.push($scope.newHouse);
-				$scope.newHouse = {};
 			});
-		};
+		}
+
 		// Edit House
 		$scope.editHouseEnabled = false;
 		$scope.editingHouse = {};
@@ -52,16 +58,21 @@ angular
 			$scope.editingHouse = house;
 		};
 
-		$scope.saveChanges = function(){
-			if (!houseService.houseInputIsValid($scope.editingHouse)) {
-				return;
+		$scope.saveChanges = function(house){
+			if (houseService.houseInputIsValid(house)) {
+				updateHouse(house);
+				$scope.setAlert(false, house.Type + " " + house.ID + " updated.");
 			}
-			var houseId = $scope.editingHouse.ID;
-			houseFactory.update({id: $scope.editingHouse.ID}, $scope.editingHouse);
-			$scope.editingHouse = {};
-			$scope.editHouseEnabled = false;
+			else {
+				console.log("Invalid Input. House is not updated.")
+				$scope.setAlert(true, "Invalid Input. Adding aborted !");
+			}
 		};
 
+		function updateHouse(house) {
+			var houseId = house.ID;
+			houseFactory.update({id: house.ID}, house);
+		}
 		// Delete House
 		$scope.deleteHouse = function(house){
 			// Remove from database
@@ -69,19 +80,36 @@ angular
 				// Remove from page
 				var index = $scope.houses.indexOf(house);
 				$scope.houses.splice(index, 1);
+				$scope.setAlert(false, house.Type + " " + house.ID + " removed.");
 			})
 		};
 
 		// Filter Houses
-		var min = 0, max = -1;
+		var min = 0, max = 0;
 
 		$scope.resetFilter = function(){
 			$scope.priceInfo = {
 				min: min,
 				max: max
-			}
+			};
 		};
+
 		$scope.resetFilter();
 
+		// Notification Alert
+		$scope.alertMessage = "";
+		$scope.alertOn = false;
+		$scope.alertClass = "alert-info";
 
+		$scope.setAlert = function(warning, content) {
+			if (warning) {
+				$scope.alertClass = "alert-danger";
+			}
+			else {
+				$scope.alertClass = "alert-success";
+			}
+
+			$scope.alertMessage = content;
+			$scope.alertOn = true;
+		};
 	});
