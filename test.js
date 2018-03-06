6affea8c8e0aecd4d6790b61033315221765197e
eragonwien <nguyennhoangson@gmail.com>
2018-03-06 10:16:10 +0700

    add test file

---
+++ test.js
@@ -0,0 +1,7 @@
+process.env.NODE_ENV = 'test';
+var addressTest = require('./components/address/addressTest');  
+var currencyTest = require('./components/currency/currencyTest');
+var houseTypeTest = require('./components/house_type/houseTypeTest');
+var roleTest = require('./components/role/roleTest');
+var userTest = require('./components/user/userTest');
+
