2459bada533a1ee8e6e6a8cdc3f55170cf1dc410
eragonwien <nguyennhoangson@gmail.com>
2018-03-06 10:16:49 +0700

    add mvc for address

---
+++ components/address/addressModel.js
@@ -0,0 +1,68 @@
+var pool = require('../../config/db').pool;
+
+exports.createNewAddress = function (address, done) {
+    var cmd = 'INSERT INTO address(address, postal_code, city, land) VALUES (?, ?, ?, ?) ';
+    cmd += 'ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id);';
+    var params = [address.address, address.postal_code, address.city, address.land];
+    pool.query(cmd, params, function (error, result) {
+        if (error) {
+            return done(error);
+        }
+        done(null, result);
+    })
+};
+
+exports.getAllAddresses = function (done) {
+    var cmd = 'SELECT * FROM address;'
+    pool.query(cmd, null, function (error, results) {
+        if (error) {
+            return done(error);
+        }
+        done(null, results);
+    })
+};
+
+exports.getAddressById = function (id, done) {
+    var cmd = 'SELECT * FROM address WHERE id=? LIMIT 1;';
+    var params = [id];
+    pool.query(cmd, params, function (error, result) {
+        if (error) {
+            return done(error);
+        }
+        done(null, result[0]);
+    })
+};
+
+exports.getAddressIdByAddress = function (address, done) {
+    var cmd = 'SELECT id FROM address WHERE address=? AND postal_code=? AND city=? AND land=? LIMIT 1;';
+    var params = [address.address, address.postal_code, address.city, address.land];
+    pool.query(cmd, params, function (error, result) {
+        if (error) {
+            return done(error);
+        }
+        done(null, result[0]);
+    })
+};
+
+exports.updateAddressById = function (id, address, done) {
+    var cmd = 'UPDATE address ';
+    cmd += 'SET address=?, postal_code=?, city=?, land=? WHERE id=?;';
+    var params = [address.address, address.postal_code, address.city, address.land, id];
+    pool.query(cmd, params, function (error, result) {
+        if (error) {
+            return done(error);
+        }
+        done(null, result);
+    })
+};
+
+exports.deleteAddressById = function (id, done) {
+    var cmd = 'DELETE FROM address WHERE id=?;';
+    var params = [id];
+    pool.query(cmd, params, function (error, result) {
+        if (error) {
+            return done(error);
+        }
+        done(null, result);
+    })
+};
---
+++ components/address/addressTest.js
@@ -0,0 +1,172 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../../app');
+var should = chai.should;
+var expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Address Model Test', function () {
+    var address = {
+        id: null,
+        address: 'Neubaugasse 73',
+        postal_code: '1070',
+        city: 'Vienna',
+        land: 'Austria'
+    }
+    var model = require('./addressModel');
+    it('should create a new address and return an id', function (done) {
+        model.createNewAddress(address, function (error, result) {
+            if (error) {
+                return done(error);
+            }
+            expect(result).to.have.property('insertId');
+            address.id = result.insertId;
+            done();
+        })
+    });
+    it('should get all addresses', function (done) {
+        model.getAllAddresses(function (error, results) {
+            if (error) {
+                return done(error);
+            }
+            expect(results).to.be.an('array');
+            expect(results.length).to.be.greaterThan(0);
+            done();
+        })
+    });
+    it('should get address by id', function (done) {
+        model.getAddressById(address.id, function (error, result) {
+            if (error) {
+                return done(error);
+            }
+            expect(result).to.be.an('object');
+            expect(result).to.have.property('address').which.is.equal(address.address);
+            expect(result).to.have.property('postal_code').which.is.equal(address.postal_code);
+            expect(result).to.have.property('city').which.is.equal(address.city);
+            expect(result).to.have.property('land').which.is.equal(address.land);
+            done();
+        })
+    });
+    it('should get id of an address', function (done) {
+        model.getAddressIdByAddress(address, function (error, result) {
+            if (error) {
+                return done(error);
+            }
+            expect(result).to.be.an('object');
+            expect(result).to.have.property('id').which.is.equal(address.id);
+            done();
+        })
+    });
+    it('should update an address', function (done) {
+        address.address = 'Neubaugasse 84';
+        address.city = 'Wien';
+        model.updateAddressById(address.id, address, function (error, result) {
+            if (error) {
+                return done(error);
+            }
+            expect(result).to.be.an('object');
+            model.getAddressById(address.id, function (error, result) {
+                if (error) {
+                    return done(error);
+                }
+                expect(result).to.be.an('object');
+                expect(result).to.have.property('address').which.is.equal(address.address);
+                expect(result).to.have.property('postal_code').which.is.equal(address.postal_code);
+                expect(result).to.have.property('city').which.is.equal(address.city);
+                expect(result).to.have.property('land').which.is.equal(address.land);
+                done();
+            })
+        })
+    });
+    it('should remove an address', function (done) {
+        model.deleteAddressById(address.id, function (error, result) {
+            if (error) {
+                return done(error);
+            }
+            model.getAddressById(address.id, function (error, result) {
+                if (error) {
+                    return done(error);
+                }
+                expect(result).to.be.undefined;
+                done();
+            })
+        })
+    });
+});
+
+describe('Address request Test', function () {
+    var address = {
+        id: null,
+        address: 'Neubaugasse 73',
+        postal_code: '1070',
+        city: 'Vienna',
+        land: 'Austria'
+    };
+    it('should create new address per POST on api/addresses', function (done) {
+        chai.request(app)
+            .post('/api/addresses')
+            .send(address)
+            .end(function (error, result) {
+                expect(result).to.have.status(200);
+                expect(result.body).to.have.property('insertId');
+                address.id = result.body.insertId;
+                done();
+            });
+    });
+    it('should get all addresses per GET on api/addresses', function (done) {
+        chai.request(app)
+            .get('/api/addresses')
+            .end(function (error, result) {
+                expect(result).to.have.status(200);
+                expect(result.body).to.be.an('array');
+                done();
+            });
+    });
+    it('should get address by id per GET on api/addresses/:id', function (done) {
+        chai.request(app)
+            .get('/api/addresses/' + address.id)
+            .end(function (error, result) {
+                expect(result).to.have.status(200);
+                expect(result.body).to.be.an('object');
+                expect(result.body).to.have.property('address').which.is.equal(address.address)
+                expect(result.body).to.have.property('postal_code').which.is.equal(address.postal_code)
+                expect(result.body).to.have.property('city').which.is.equal(address.city)
+                expect(result.body).to.have.property('land').which.is.equal(address.land)
+                done();
+            });
+    });
+    it('should get address id per POST on api/addresses/id', function (done) {
+        chai.request(app)
+            .post('/api/addresses/id')
+            .send(address)
+            .end(function (error, result) {
+                expect(result).to.have.status(200);
+                expect(result.body).to.be.an('object');
+                expect(result.body).to.have.property('id').which.is.equal(address.id);
+                done();
+            });
+    });
+    it('should update address per PUT on api/addresses/:id', function (done) {
+        chai.request(app)
+            .put('/api/addresses/' + address.id)
+            .send(address)
+            .end(function (error, result) {
+                expect(result).to.have.status(200);
+                done();
+            })
+    });
+    it('should delete address per DELETE on api/addresses/:id', function (done) {
+        chai.request(app)
+            .delete('/api/addresses/' + address.id)
+            .end(function (error, result) {
+                expect(result).to.have.status(200);
+                chai.request(app)
+                    .get('/api/addresses/' + address.id)
+                    .end(function (error, result) {
+                        expect(result).to.have.status(200);
+                        expect(result.body).to.be.empty;
+                        done();
+                    });
+            });
+    });
+})
