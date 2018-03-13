2db9edb48666a24b42ecb8c4944212e13ebf35c8
eragonwien <nguyennhoangson@gmail.com>
2018-03-13 22:05:55 +0700

    add app module and routes

---
+++ public/javascripts/angular/app.routes.js
@@ -0,0 +1,124 @@
+angular
+    .module('house')
+    .config(routing);
+
+routing.$inject = ['$stateProvider', '$urlRouterProvider'];
+function routing(stateProvider, urlRouterProvider) {
+    var index = {
+        name: 'index',
+        url: '/',
+        views: {
+            navbar: {
+                templateUrl: 'javascripts/angular/navbar/navbar.view.html',
+                controller: 'navbarController',
+                controllerAs: 'navbar'
+            },
+            content: {
+                templateUrl: 'javascripts/angular/house/index/index.view.html',
+                controller: 'indexController',
+                controllerAs: 'index'
+            }
+        }
+    }
+
+    var signup = {
+        name: 'signup',
+        url: '/signup',
+        views: {
+            navbar: {
+                templateUrl: 'javascripts/angular/navbar/navbar.view.html',
+                controller: 'navbarController',
+                controllerAs: 'navbar'
+            },
+            content: {
+                templateUrl: 'javascripts/angular/user/signup/signup.view.html',
+                controller: 'signupController',
+                controllerAs: 'signup'
+            }
+        }
+    }
+
+    var login = {
+        name: 'login',
+        url: '/login',
+        views: {
+            navbar: {
+                templateUrl: 'javascripts/angular/navbar/navbar.view.html',
+                controller: 'navbarController',
+                controllerAs: 'navbar'
+            },
+            content: {
+                templateUrl: 'javascripts/angular/user/login/login.view.html',
+                controller: 'loginController',
+                controllerAs: 'login'
+            }
+        }
+    }
+    var logout = {
+        name: 'logout',
+        url: '/logout',
+        views: {
+            navbar: {
+                templateUrl: 'javascripts/angular/navbar/navbar.view.html',
+                controller: 'navbarController',
+                controllerAs: 'navbar'
+            },
+            content: {
+                templateUrl: 'javascripts/angular/user/logout/logout.view.html',
+                controller: 'logoutController',
+                controllerAs: 'logout'
+            }
+        }
+    }
+
+    var profile = {
+        name: 'profile',
+        url: '/profile',
+        views: {
+            navbar: {
+                templateUrl: 'javascripts/angular/navbar/navbar.view.html',
+                controller: 'navbarController',
+                controllerAs: 'navbar'
+            },
+            content: {
+                templateUrl: 'javascripts/angular/user/profile/profile.view.html',
+                controller: 'profileController',
+                controllerAs: 'profile'
+            }
+        }
+    }
+
+    var createHouse = {
+        name: 'createHouse',
+        url: '/houses/create',
+        views: {
+            navbar: {
+                templateUrl: 'javascripts/angular/navbar/navbar.view.html',
+                controller: 'navbarController',
+                controllerAs: 'navbar'
+            },
+            content: {
+                templateUrl: 'javascripts/angular/house/create/create.house.view.html',
+                controller: 'createHouseController',
+                controllerAs: 'createHouse'
+            }
+        }
+    }
+
+    stateProvider.state(index);
+    stateProvider.state(signup);
+    stateProvider.state(login);
+    stateProvider.state(logout);
+    stateProvider.state(profile);
+    stateProvider.state(createHouse);
+
+    urlRouterProvider.otherwise('/login');
+};
+
+checkUser.$inject = ['$q', '$location', '$userService']
+function checkUser(q, location, userService) {
+    if (!userService.getLocalUser()) {
+        location.path('/');
+        return;
+    }
+}
