d1c8d4c8272a2e2b0e69e2dae093544e658c0590
SonLaptop\erago <eragonwien@gmail.com>
2018-03-17 14:03:40 +0100

    add setup test

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 process.env.NODE_ENV = 'test';
+// API Tests
 var addressTest = require('./backend/components/address/addressTest');  
 var currencyTest = require('./backend/components/currency/currencyTest');
 var houseTest = require('./backend/components/house/houseTest');
 var roleTest = require('./backend/components/role/roleTest');
 var userTest = require('./backend/components/user/userTest');
 
+// Setup Test
+var setupTest = require('./setup/setupTest');
