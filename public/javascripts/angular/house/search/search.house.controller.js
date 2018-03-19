63b8291d22ac1341b15382d9a6d5eafbb4269a99
eragonwien <nguyennhoangson@gmail.com>
2018-03-19 21:49:40 +0100

    add search house view and controller

--- public/javascripts/angular/house/search/search.house.controller.js
+++ public/javascripts/angular/house/search/search.house.controller.js
@@ -2,22 +2,53 @@ angular
     .module('house')
     .controller('searchHouseController', searchHouseController);
 
-searchHouseController.$inject = ['houseTypes', 'appService']
-function searchHouseController(houseTypes, appService) {
+searchHouseController.$inject = ['houseTypes', 'currencies', 'appService', 'houseService']
+function searchHouseController(houseTypes, currencies, appService, houseService) {
     var vm = this;
     vm.houseTypes = houseTypes;
+    vm.currencies = currencies;
     vm.submit = submit;
-
+    vm.resetFilter = resetFilter;
     /**
      * submits a form
-     * @param {object} form search form
      * @param {object} house search house
      */
-    function submit(form, house) {
-        alert(JSON.stringify(house));
+    function submit(house) {
+        closeCollapsible();
+        vm.loading = true;
+        splitAddress(house);
+        houseService.getHouses({search: house}).then(getHousesHandler);
+
+        function getHousesHandler(response) {
+            vm.results = response.data;
+            vm.loading = false;
+        }
     }
 
     function alert(message, duration) {
         appService.alert(message, duration);
+    }
+
+    function splitAddress(house) {
+        if (!house.address) {
+            return;
+        }
+        var address = house.address.split(',');
+        house.city = address[0];
+        house.land = address[1];
+        delete house.address;
+    }
+
+    function closeCollapsible() {
+        $('.collapsible').collapsible('close', 0);
+    }
+
+    function resetFilter() {
+        vm.house = {
+            minSize: 0,
+            minRooms: 0,
+            bathrooms: 0,
+            bedrooms: 0
+        }
     }
 }
