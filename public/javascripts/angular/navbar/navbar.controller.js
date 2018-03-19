d2e4df31f8af085e3bb81d26cc6d23f0c0ae7d69
eragonwien <nguyennhoangson@gmail.com>
2018-03-19 21:49:27 +0100

    move service to app service

--- public/javascripts/angular/navbar/navbar.controller.js
+++ public/javascripts/angular/navbar/navbar.controller.js
@@ -2,19 +2,19 @@ angular
     .module('house')
     .controller('navbarController', navbarController);
 
-navbarController.$inject = ['userService', '$location', '$window']
-function navbarController(userService, location, window) {
+navbarController.$inject = ['userService', 'appService']
+function navbarController(userService, appService) {
     var vm = this;
     vm.user = userService.getLocalUser();
     vm.reload = reload;
     vm.logout = logout;
 
     function reload() {
-        window.location.reload();
+        appService.reload();
     }
 
     function logout() {
         userService.deleteLocalUser();
-        location.url('/logout');
+        appService.moveTo('redirect');
     }
 }
