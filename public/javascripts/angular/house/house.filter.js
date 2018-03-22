bf8d7f8eb9395c8abcd8edb130b8a54c24ae998a
SonLaptop\erago <eragonwien@gmail.com>
2018-03-22 18:04:20 +0100

    add house filter

---
+++ public/javascripts/angular/house/house.filter.js
@@ -0,0 +1,23 @@
+angular
+    .module('house')
+    .filter('houseFilter', houseFilter);
+
+function houseFilter() {
+
+    /**
+     * filter out all houses which belong to the user
+     * @param {object[]} houses list of houses
+     * @param {object} user user object
+     */
+    function filter(houses, user) {
+        var result = [];
+        houses.forEach(function (house) {
+            if (user && user.id != house.user_id) {
+                result.push(house);
+            }
+        });
+        return result;
+    }
+
+    return filter;
+}
