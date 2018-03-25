5555f28eb605f79050920b5e44d74ae1382725e5
SonLaptop\erago <eragonwien@gmail.com>
2018-03-25 17:35:35 +0200

    add check for protected route

--- backend/components/routes.js
+++ backend/components/routes.js
@@ -9,7 +9,9 @@ var currency = require('./currency/currencyController');
 var role = require('./role/roleController');
 var houseType = require('./houseType/houseTypeController');
 var houseStatus = require('./houseStatus/houseStatusController');
-var session = require('../config/session');
+
+var checkUser = user.checkUser;
+var checkAdmin = user.checkAdmin;
 // Authentication & Redirection
 router.get('/', function (req, res, next) {
     res.render('index');
@@ -19,52 +21,52 @@ router.get('/logout', user.logout);
 
 // User
 router.post('/api/users', user.createUser);
-router.get('/api/users', user.getAllUsers);
-router.get('/api/users/:uid', user.getUserById);
-router.put('/api/users/:uid', user.updateUserbyId);
-router.delete('/api/users/:uid', user.deleteUserById);
+router.get('/api/users',checkAdmin, user.getAllUsers);
+router.get('/api/users/:uid',checkUser, user.getUserById);
+router.put('/api/users/:uid',checkUser, user.updateUserbyId);
+router.delete('/api/users/:uid',checkUser, user.deleteUserById);
 
 // Address
-router.post('/api/addresses', address.createAddress);
-router.get('/api/addresses', address.getAllAddresses);
-router.get('/api/addresses/:aid', address.getAddressById);
-router.post('/api/addresses/id', address.getAddressIdByAddress);
-router.put('/api/addresses/:aid', address.updateAddressById);
-router.delete('/api/addresses/:aid', address.deleteAddressById);
+router.post('/api/addresses',checkUser, address.createAddress);
+router.get('/api/addresses',checkUser, address.getAllAddresses);
+router.get('/api/addresses/:aid',checkUser, address.getAddressById);
+router.post('/api/addresses/id',checkUser, address.getAddressIdByAddress);
+router.put('/api/addresses/:aid',checkUser, address.updateAddressById);
+router.delete('/api/addresses/:aid',checkUser, address.deleteAddressById);
 
 // Currency
-router.post('/api/currencies', currency.createCurrency);
-router.get('/api/currencies', currency.getAllCurrencies);
-router.get('/api/currencies/:cid', currency.getCurrencyById);
-router.put('/api/currencies/:cid', currency.updateCurrency);
-router.delete('/api/currencies/:cid', currency.deleteCurrency);
+router.post('/api/currencies',checkUser, currency.createCurrency);
+router.get('/api/currencies',checkUser, currency.getAllCurrencies);
+router.get('/api/currencies/:cid',checkUser, currency.getCurrencyById);
+router.put('/api/currencies/:cid',checkUser, currency.updateCurrency);
+router.delete('/api/currencies/:cid',checkUser, currency.deleteCurrency);
 
 // Role
-router.post('/api/roles', role.createRole);
-router.get('/api/roles', role.getAllRoles);
-router.get('/api/roles/:rid', role.getRoleById);
-router.put('/api/roles/:rid', role.updateRoleById);
-router.delete('/api/roles/:rid', role.deleteRoleById);
+router.post('/api/roles',checkUser, role.createRole);
+router.get('/api/roles',checkUser, role.getAllRoles);
+router.get('/api/roles/:rid',checkUser, role.getRoleById);
+router.put('/api/roles/:rid',checkUser, role.updateRoleById);
+router.delete('/api/roles/:rid',checkUser, role.deleteRoleById);
 
 // House
 router.post('/api/houses', house.createHouse);
 router.get('/api/houses', house.getHouses);
-router.get('/api/houses/:hid', house.getHouseById);
-router.put('/api/houses/:hid', house.updateHouseById);
-router.delete('/api/houses/:hid', house.deleteHouseById);
+router.get('/api/houses/:hid',checkUser, house.getHouseById);
+router.put('/api/houses/:hid',checkUser, house.updateHouseById);
+router.delete('/api/houses/:hid',checkUser, house.deleteHouseById);
 
 // House Type
-router.post('/api/houseTypes', houseType.createHouseType);
-router.get('/api/houseTypes', houseType.getAllHouseType);
-router.get('/api/houseTypes/:htid', houseType.getHouseTypeById);
-router.put('/api/houseTypes/:htid', houseType.updateHouseTypeById);
-router.delete('/api/houseTypes/:htid', houseType.deleteHouseTypeById);
+router.post('/api/houseTypes',checkUser, houseType.createHouseType);
+router.get('/api/houseTypes',checkUser, houseType.getAllHouseType);
+router.get('/api/houseTypes/:htid',checkUser, houseType.getHouseTypeById);
+router.put('/api/houseTypes/:htid',checkUser, houseType.updateHouseTypeById);
+router.delete('/api/houseTypes/:htid',checkUser, houseType.deleteHouseTypeById);
 
 // House Status
-router.post('/api/houseStatuses', houseStatus.createHouseStatus);
-router.get('/api/houseStatuses', houseStatus.getAllHouseStatuses);
-router.get('/api/houseStatuses/:hsid', houseStatus.getHouseStatusById);
-router.put('/api/houseStatuses/:hsid', houseStatus.updateHouseStatusById);
-router.delete('/api/houseStatuses/:hsid', houseStatus.deleteHouseStatusById);
+router.post('/api/houseStatuses',checkUser, houseStatus.createHouseStatus);
+router.get('/api/houseStatuses',checkUser, houseStatus.getAllHouseStatuses);
+router.get('/api/houseStatuses/:hsid',checkUser, houseStatus.getHouseStatusById);
+router.put('/api/houseStatuses/:hsid',checkUser, houseStatus.updateHouseStatusById);
+router.delete('/api/houseStatuses/:hsid',checkUser, houseStatus.deleteHouseStatusById);
 
 module.exports = router;
