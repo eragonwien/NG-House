80216b6e8b65f9a9d083694db184e4e62c4a94be
SonLaptop\erago <eragonwien@gmail.com>
2018-03-28 19:45:26 +0200

    revert moving the separating the server

--- server/front/app.module.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('app', []);
-app.config(appConfig);
-
-appConfig.$inject = ['$httpProvider']
-function appConfig(httpProvider) {
-    httpProvider.defaults.withCredentials = true;
-}
