945df85a50b3bec486abcc3c2ac635fe9b4f06a2
eragonwien <nguyennhoangson@gmail.com>
2018-04-03 12:54:30 +0200

    add helper

--- build/setup.js
+++ build/setup.js
@@ -1,7 +1,12 @@
-const ENV_PATH = '.env';
-
+'use strict';
 require('dotenv').config();
 let fs = require('fs');
+let readline = require('readline-sync');
+
+const ENV_PATH = '.env';
+const SQL_CREATE_PATH = 'backend/config/sql/create.sql';
+const SQL_INSERT_PATH = 'backend/config/sql/insert.sql';
+const SQL_VIEWS_PATH = 'backend/config/sql/views.sql';
 
 start(function (result) {
     if (result) {
@@ -18,9 +23,9 @@ function start(done) {
                 console.log(element);
             }
         }
-        return done('Argument catched.')
+        return done('Argument catched.');
     }
-    if (fs.readFileSync(ENV_PATH)) {
+    if (fs.existsSync(ENV_PATH)) {
         console.log('File .env exists.');
         checkEnvVariables(done);
         return;
@@ -69,7 +74,7 @@ function createEnv() {
 
     console.log('\n' + env);
     if (readline.keyInYN('Save as .env ?')) {
-        fs.writeFileSync(path, env);
+        fs.writeFileSync(ENV_PATH, env);
         console.log('Saved.');
         return true;
     }
@@ -110,8 +115,70 @@ function checkEnvVariables(done) {
         missingList.push('DB_POOL_LIMIT');
     }
 
-    console.log(missingList.toString());
-    let result = (missingList.length !== 0)
-    done(result);
+    if (missingList.length > 0) {
+        fillEnvVariables(missingList, done);
+        return;
+    }
+    checkDB(done);
 }
+
+function fillEnvVariables(variables, done) {
+    console.log('Filling .env variables');
+    let envContent = fs.readFileSync(ENV_PATH);
+    for (let i = 0; i < variables.length; i++) {
+        const element = variables[i];
+        let value = readline.question('Value for ' + element);
+        envContent += (element + '=' + value + '\n');
+    }
+    checkEnvVariables(done);
+}
+
+function checkDB(done) {
+    console.log('Checking database');
+    
+    let db = require('../backend/config/db');
+    let pool = db.pool;
+    pool.query('SELECT 1 AS solution', null, function (error, result) {
+        if (error) {
+            console.log('Database does not exists');
+            createDB(done);
+            db.closePool(pool); // close pool since it is no longer used
+            return;
+        } 
+        console.log('Database exists.');
+        let databaseName = pool.config.connectionConfig.database;
+        if (readline.keyInYN('Re-create this database ? ' + databaseName)) {
+            db.closePool(pool); // close pool since it is no longer used            
+            createDB(true, done);
+            return;
+        }
+        createViews(pool, done);
+    });
+}
+
+function createDB(isReset, done) {
+    if (isReset) {
+        console.log('Recreating database');
+    } else {
+        console.log('Creating database');
+    }
+    let db = require('../backend/config/db');
+    let pool = db.pool_no_database;
+    
+    db.closePool(pool);
+    insertDB(done);
+    
+}
+
+function insertDB(done) {
+    console.log('Inserting database');
+    done();
+}
+
+function createViews(pool, done) {
+    console.log('Creating Views');
+    done();
+}
+
+
 
